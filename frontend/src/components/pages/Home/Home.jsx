b4b1381b8ba643c4cebbad4b2e4ac8f417cd9ab7
Ranga Dananjaya <rangamaithreepala@gmail.com>
2023-11-12 09:24:42 +0530

    third

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -25,8 +25,6 @@ const Home = () => {
               <Typography
                 color="blue-gray"
                 className="font-medium"
-                
-                
               >
                 UOR E-FAC
               </Typography>
